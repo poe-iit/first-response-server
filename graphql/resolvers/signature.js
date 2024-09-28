1a153e86bbfbeefbbe4e8614673301e7b09a5ef0
Tayomide <tibrahim@hawk.iit.edu>
2024-09-28 18:02:25 -0500

    Create Signature class for resolver operations

---
+++ graphql/resolvers/signature.js
@@ -0,0 +1,10 @@
+// Define a 'Signature' class to encapsulate signature-related operations and data
+class Signature{
+  constructor(signature, timeStamp){
+    this.signature = signature
+    this.timeStamp = timeStamp
+  }
+}
+
+// Export the Signature class for use in other parts of the application
+module.exports = Signature
