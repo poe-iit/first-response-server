9091fbdeea05c6e28f6323c94a95b3da812c31d7
Tayomide <tibrahim@hawk.iit.edu>
2024-09-28 18:17:12 -0500

    Implement graphql mutation resolver logic

---
+++ graphql/resolvers/mutation/createUser.js
@@ -0,0 +1,31 @@
+const { model } = require("mongoose")
+const { userSchema } = require("../../../models/user")
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const UserModel = model("User", userSchema)
+
+const User = require("../user")
+
+// Define an asynchronous function to create a user based on the provided 'email' and 'password'
+// The function expects an object with an 'email' and 'password' property
+const createUser = async ({ email, password }, context) => {
+  const existingUser = await UserModel.findOne({ email })
+
+  if(existingUser) {
+    throw new Error('Email is currently in use' )
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const newUser = await UserModel.create({ email, password: hashedPassword, roles: ["user"] })
+
+  const token = jwt.sign({ userId: newUser.id, roles: ["user"] }, process.env.ACCESS_SECRET, { expiresIn: '3d' })
+
+  context.response.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' })
+
+  return new User(newUser)
+}
+
+// Export the 'createUser' function to make it accessible from other modules
+module.exports = { createUser }
