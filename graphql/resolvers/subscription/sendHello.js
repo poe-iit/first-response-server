94b7d599270288171a80f6f812c24757c438df76
Tayomide <tibrahim@hawk.iit.edu>
2024-10-01 00:11:24 -0500

    Add authorisation to websocket endpoint

--- graphql/resolvers/subscription/sendHello.js
+++ graphql/resolvers/subscription/sendHello.js
@@ -1,8 +1,12 @@
 const pubsub = require("../../../helpers/pubsub")
+const { withAuthorisation } = require("../../../helpers/withAuthorisation")
 
 // Define a function that returns an async iterator for the "SEND_HELLO" event
 // This allows clients to subscribe to the "SEND_HELLO" event and receive messages when this event is published
-const sendHello = () => pubsub.asyncIterator("SEND_HELLO")
+const sendHello = withAuthorisation(
+  () => pubsub.asyncIterator("SEND_HELLO"),
+  ["user", "admin"]
+)
 
 // Export the 'sendHello' function for use in GraphQL resolvers or other parts of the application
 module.exports = {
