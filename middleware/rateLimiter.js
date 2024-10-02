06a337eac75f41488c804300158c88cf9fe96c44
Tayomide <tibrahim@hawk.iit.edu>
2024-10-02 18:54:44 -0500

    Implement rate limiter middle ware

---
+++ middleware/rateLimiter.js
@@ -0,0 +1,9 @@
+const { rateLimit } = require("express-rate-limit")
+
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+})
+
+module.exports = limiter
