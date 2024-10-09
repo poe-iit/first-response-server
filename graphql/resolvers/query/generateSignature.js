edf9ea1d71533c7a93473b75cf553f3e119f315a
Saad <77770818+Saad-iit@users.noreply.github.com>
2024-10-09 15:51:44 -0500

    Update generateSignature.js

--- graphql/resolvers/query/generateSignature.js
+++ graphql/resolvers/query/generateSignature.js
@@ -1,5 +1,5 @@
-const crypto = require("crypto")
-const Signature = require("../signature")
+const crypto = require("crypto");
+const Signature = require("../signature");
 
 // Define an asynchronous function to generate a cryptographic signature based on the provided 'id'
 // The function expects an object with an 'id' property
@@ -11,19 +11,21 @@ const generateSignature = async ({ id }, context) => {
   const params_to_sign = {
     public_id: id,
     timestamp
-  }
+  };
 
   const param_string = Object.keys(params_to_sign)
     .sort()
     .map(key => `${key}=${params_to_sign[key]}`)
-    .join("&")
+    .join("&");
 
-  const signature = crypto.createHash("sha1").update(param_string+process.env.CLOUDINARY_SECRET).digest("hex")
+  // Using SHA-256 for hashing instead of SHA-1
+  const signature = crypto.createHash("sha256").update(param_string + process.env.CLOUDINARY_SECRET).digest("hex");
+
 
   const response = new Signature({signature, timeStamp: timestamp}, context)
-  return response
-}
+  return response;
+};
 
 
 // Export the 'generateSignature' function to make it accessible from other modules
-module.exports = { generateSignature }
+module.exports = { generateSignature };
