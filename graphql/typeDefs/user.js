b4048a6e37fac260bcd2523f4dc371b3b9674251
Tayomide <tibrahim@hawk.iit.edu>
2024-10-12 03:48:55 -0500

    Add accountStatus field to graphQL schema

--- graphql/typeDefs/user.js
+++ graphql/typeDefs/user.js
@@ -1,11 +1,13 @@
 const userSchema = `
   type User {
+    id: ID!
     username: String
     email: String!
     roles: [String]!
     buildings: [Building]!
-    createdAt: String
-    updatedAt: String
+    createdAt: String!
+    updatedAt: String!
+    accountStatus: String!
   }
 `
 
