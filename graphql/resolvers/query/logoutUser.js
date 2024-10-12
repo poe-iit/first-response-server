4a842f356db429679417ae338b83f22faa77f711
Tayomide <tibrahim@hawk.iit.edu>
2024-10-12 18:08:53 -0500

    Define initial logout logic

---
+++ graphql/resolvers/query/logoutUser.js
@@ -0,0 +1,24 @@
+// Make a more sophisicated model where tokens are stored in the database and can be refreshed/invalidated
+const Status = require("../status")
+
+// Define an asynchronous function to authenticate a user based on the provided 'email' and 'password'
+// The function expects an object with an 'email' and 'password' property
+const logoutUser = async (_, context) => {
+  console.log("Called")
+  context.isAuth = false
+  context.user = null
+  context.response.cookie('token', "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSide: "None",
+    expires: new Date(0)
+  })
+  
+  return new Status({
+    message: "Logged out successfully",
+    status: 200
+  })
+}
+
+// Export the 'loginUser' function to make it accessible from other modules
+module.exports = { logoutUser }
