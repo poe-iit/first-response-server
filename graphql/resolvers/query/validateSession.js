50d31b3867045e1e899fb04c51d92827ce78c24f
Tayomide <tibrahim@hawk.iit.edu>
2024-10-12 15:47:39 -0500

    Update error message

--- graphql/resolvers/query/validateSession.js
+++ graphql/resolvers/query/validateSession.js
@@ -1,7 +1,7 @@
 const User = require("../user")
 
 const validateSession = async ({}, context) => {
-  if(!context?.isAuth) throw new Error("Error updating Building. You are not authenticated.")
+  if(!context?.isAuth) throw new Error("Error validating session. You are not authenticated.")
   
   const user = await User.build(context.user.userId, context)
   return user
