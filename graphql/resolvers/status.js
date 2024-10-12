f8cbbb4e672057568d53d1b00a506be86737b68c
Tayomide <tibrahim@hawk.iit.edu>
2024-10-12 18:07:19 -0500

    Create status resolver

---
+++ graphql/resolvers/status.js
@@ -0,0 +1,9 @@
+class Status{
+  constructor({message, status}){
+    this.message = message
+    this.status = status
+  }
+}
+
+// Export the Signature class for use in other parts of the application
+module.exports = Status
