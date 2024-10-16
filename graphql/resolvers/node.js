c314b1135857b3c8a4100e196d4ea153c390c0b6
Tayomide <tibrahim@hawk.iit.edu>
2024-10-16 12:22:19 -0500

    Create node resolver

--- graphql/resolvers/node.js
+++ graphql/resolvers/node.js
@@ -5,6 +5,18 @@ const NodeModel = model("Node", nodeSchema)
 
 // Define a 'Node' class to encapsulate node-related operations and data
 class Node {
+
+  static async build(nodeId, context) {
+    if(!context?.isAuth) throw new Error("Error retrieving Node data. You are not authenticated.")
+
+    const node = await NodeModel.findById(nodeId)
+    if (!node) {
+      throw new Error(`Node ${nodeId} not found`)
+    }
+
+    return new Node(node, context)
+  }
+
   constructor(node, context) {
     if(!context?.isAuth) throw new Error("Error retrieving Node data. You are not authenticated.")
     this.context = context
@@ -13,9 +25,27 @@ class Node {
     this.id = node.id || ""
     this.name = node.name || ""
     this.state = node.state
-    this.connections = node.connections || [] // Work on converting this into an array of Node
     this.isExit = node.isExit
     this.ui = node.ui
+  }
+
+  async connections() {
+    const connections = []
+    const InvisibleNode = require("./invisibleNode")
+
+    const invisibleNodes = this.node.connections
+    for(const invisibleNodeId of invisibleNodes) {
+      const invisibleNode = await InvisibleNode.build(invisibleNodeId, this.context)
+
+      const connectedNodes = await invisibleNode.connectedNodes()
+      if(connectedNodes[0].id === this.id) {
+        connections.push(connectedNodes[1])
+      }else {
+        connections.push(connectedNodes[0])
+      }
+    }
+
+    return connections
   }
 }
 
