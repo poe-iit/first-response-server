24e488ba0d65ad0acbb62a4a225d823b53a38889
Tayomide <tibrahim@hawk.iit.edu>
2024-10-16 12:25:58 -0500

    Delete create node schema

--- graphql/typeDefs/mutation/createNodes.js
+++
@@ -1,7 +0,0 @@
-const createNodesSchema = `
-  createNode(createNodeInputs: [CreateNodeInput]!, invisibleNodeInputs: [InvisibleNodeInput]!): [Node]
-`
-
-module.exports = {
-  schema: createNodesSchema
-}
