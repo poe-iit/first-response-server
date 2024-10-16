e497a47547bf5728813d7c534cc1fbe902b6235a
Tayomide <tibrahim@hawk.iit.edu>
2024-10-16 12:35:56 -0500

    Concatenate schemas

--- graphql/typeDefs/schema.js
+++ graphql/typeDefs/schema.js
@@ -2,16 +2,17 @@ const { buildSchema } = require("graphql");
 
 const createBuilding = require("./mutation/createBuilding")
 const createFloor = require("./mutation/createFloor")
-const createNode = require("./mutation/createNode")
 const createUser = require("./mutation/createUser")
 const updateBuilding = require("./mutation/updateBuilding")
 const updateFloor = require("./mutation/updateFloor")
+const updateFloorPlan = require("./mutation/updateFloorPlan")
 const updateNode = require("./mutation/updateNode")
 
 const generateSignature = require("./query/generateSignature");
 const getBuilding = require("./query/getBuilding")
 const getBuildings = require("./query/getBuildings")
 const getFloor = require("./query/getFloor")
+const getFloorPlan = require("./query/getFloorPlan")
 const getFloors = require("./query/getFloors")
 const loginUser = require("./query/loginUser")
 const logoutUser = require("./query/logoutUser");
@@ -22,8 +23,10 @@ const sendHello = require("./subscription/sendHello")
 
 const building = require("./building")
 const floor = require("./floor")
+const floorPlan = require("./floorPlan")
 const floorImage = require("./floorImage")
 const hello = require("./hello")
+const invisibleNode = require("./invisibleNode")
 const node = require("./node")
 const nodeUI = require("./nodeUI")
 const signature = require("./signature");
@@ -41,10 +44,10 @@ const schema = buildSchema(`
   type Mutation {
     ${createBuilding.schema}
     ${createFloor.schema}
-    ${createNode.schema}
     ${createUser.schema}
     ${updateBuilding.schema}
     ${updateFloor.schema}
+    ${updateFloorPlan.schema}
     ${updateNode.schema}
   }
 
@@ -53,6 +56,7 @@ const schema = buildSchema(`
     ${getBuilding.schema}
     ${getBuildings.schema}
     ${getFloor.schema}
+    ${getFloorPlan.schema}
     ${getFloors.schema}
     ${loginUser.schema}
     ${validateSession.schema}
@@ -69,7 +73,8 @@ const schema = buildSchema(`
   ${floorImageInput.schema}
   ${floorInput.createFloorInput.schema}
   ${floorInput.updateFloorInput.schema}
-  ${invisibleNodeInput.schema}
+  ${invisibleNodeInput.createInvisibleNodeInput.schema}
+  ${invisibleNodeInput.updateInvisibleNodeInput.schema}
   ${nodeInput.createNodeInput.schema}
   ${nodeInput.nodeReferenceInput.schema}
   ${nodeInput.updateNodeInput.schema}
@@ -77,8 +82,10 @@ const schema = buildSchema(`
 
   ${building.schema}
   ${floor.schema}
+  ${floorPlan.schema}
   ${floorImage.schema}
   ${hello.schema}
+  ${invisibleNode.schema}
   ${node.schema}
   ${nodeUI.schema}
   ${signature.schema}
